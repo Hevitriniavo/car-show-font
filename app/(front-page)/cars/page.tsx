ccf9539a91dcc2f9a621ead4ab2f917dbbfc3e01
Hevitriniavo <hei.tantely@gmail.com>
2024-05-29 13:57:41 +0300

    chore: test/tesr SSR

--- app/(front-page)/cars/page.tsx
+++ app/(front-page)/cars/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+interface Data {
+    id: number,
+    title: string
+};
 
-function page() {
+const getData = async () => {
+    const json = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data: Data[] = await json.json();
+    return data;
+}
+
+
+async function page() {
+    const cars = await getData();
     return (
         <>
-            
+            {cars.map((car) => (
+                <div key={car.id}>
+                    <span>{car.id}</span>
+                    <span>{car.title}</span>
+                </div>
+            ))}
         </>
     );
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
-const AdminApp = dynamic(() => import("../ui/AdminApp"), { ssr: false });
+const AdminApp = dynamic(() => import("../components/AdminApp"), { ssr: false });
 const Home: NextPage = () => <AdminApp />;
 export default Home;
